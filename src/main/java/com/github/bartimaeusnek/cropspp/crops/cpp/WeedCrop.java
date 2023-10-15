0073bc8212fd0ec2cae0eb9f35ac5d100e3235ff
Guillaume Mercier <10gui-gui10@live.ca>
2023-10-15 15:43:15 -0400

    Mark unused Weed Crop as deprecated

--- src/main/java/com/github/bartimaeusnek/cropspp/crops/cpp/WeedCrop.java
+++ src/main/java/com/github/bartimaeusnek/cropspp/crops/cpp/WeedCrop.java
@@ -1,5 +1,7 @@
 package com.github.bartimaeusnek.cropspp.crops.cpp;
 
+// just use the vanilla weed crop instead at this point
+@Deprecated
 public class WeedCrop extends ic2.core.crop.CropWeed {
 
     public WeedCrop() {
